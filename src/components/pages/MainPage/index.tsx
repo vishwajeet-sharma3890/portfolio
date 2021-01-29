ed4d84d3849605e990c66d1b8caeaa074d650f32
buyceps-vishu <65203515+buyceps-vishu@users.noreply.github.com>
2021-01-29 23:29:27 +1000

    Removing review section temporarily

--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -68,8 +68,6 @@ export const MainPage: React.FC = () => {
       <ProjectSection />
       <QuoteSection />
       <Spacer size={`${sectionSpacer * 2}px`} type="horizontal" />
-      <ReviewSection />
-      <Spacer size={`${sectionSpacer * 2}px`} type="horizontal" />
       <ContactSection />
       <footer>
         <ProfileIcons icons={icons} defaultColor={theme.colors.white} />
